fbf8b3f9e2bcd5a9ccdef8b07873051b8e1ea413
L-Alonso <Alonso_l@icloud.com>
2021-09-12 02:36:35 -0700

    player's name fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -57,6 +57,7 @@ var fight = function(enemy){
         console.log(playerInfo);
         if (playerInfo.health >0){
             window.alert("Welcome to Robot Gladiators! " + (i + 1));
+            
         
         var pickedEnemyObj = enemyInfo[i];
             pickedEnemyObj.health= randomNumber(40, 60);
@@ -126,8 +127,17 @@ var fight = function(enemy){
            }
         };
 
+        var getPlayerName = function(){
+            var name = "";
+            while (name ==="" || name === null){
+                name = prompt("What is your robot's name?");
+            }
+            console.log("Your robot's name is " +name);
+            return name;
+        };
+
         var playerInfo = {
-            name: window.prompt("What is your robot's name?"),
+            name: getPlayerName(),
             health: 100,
             attack: 10,
             money: 10,
